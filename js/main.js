7d81fbeacb000152becd867a949d5f953e88951d
maisa abusalem <heart.webd@gmail.com>
2022-02-16 08:40:54 +0100

    create element js

--- js/main.js
+++ js/main.js
@@ -4,8 +4,14 @@
  * så skall texten från http://codexplained.se/lorem_text.php hämtas och visas på sidan.
  * Hämta texten med hjälp av AJAX, se kodgenomgången js-ajax-intro
  */
-let btn = document.getElementById('btn');
-btn.addEventListener('click', function () {
+
+let fetchDiv = document.getElementById('fetchDiv');
+let fetchBtn = document.createElement('button');
+fetchBtn.innerHTML = 'Fetch data';
+let fetchText = document.createElement('div');
+fetchDiv.append(fetchBtn, fetchText);
+
+fetchBtn.addEventListener('click', function () {
     fetchData('http://codexplained.se/lorem_text.php');
 })
 
@@ -17,19 +23,18 @@ function fetchData(text) {
 
         })
         .then(data => {
-            let dataDiv = document.getElementById('text');
-            dataDiv.innerHTML = data;
-            dataDiv.style.border = '2px solid grey';
-            dataDiv.style.padding = '5px';
-            dataDiv.style.margin = '5px';
+           
+            fetchText.innerHTML = data;
+            fetchText.style.border = '2px solid grey';
+            fetchText.style.padding = '5px';
+            fetchText.style.margin = '5px';
 
         })
         .catch(error => {
-            errorMessage = 'File not found...';
-            let errorDiv = document.getElementById('text');
-            errorDiv.style.color = 'red';
-            errorDiv.style.fontSize = '20px';
-            errorDiv.style.margin = '5px';
-            errorDiv.innerHTML = errorMessage;
+            errorMessage = 'Error 404: File not found...';
+            fetchText.style.color = 'red';
+            fetchText.style.fontSize = '20px';
+            fetchText.style.margin = '5px';
+            fetchText.innerHTML = errorMessage;
         })
 }
